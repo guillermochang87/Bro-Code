42f9ca4c2e63ebe71694af4b0bd93b977e6f325c
Guillermo Chang <guillermo.chang87@gmail.com>
2025-12-11 11:22:00 -0600

    LAST MERGE

---
+++ src/com/stack/example/StackExample.java
@@ -0,0 +1,36 @@
+package com.stack.example;
+
+import java.util.Stack;
+
+public class StackExample {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+
+        
+        System.out.println("Is Stack empty?: " + stack.isEmpty());
+
+        stack.push("Skyrim");
+        stack.push("Doom");
+        stack.push("Borderlands");
+
+        System.out.println(stack);
+        System.out.println("Is Stack empty?: " + stack.isEmpty());
+
+        // stack.pop();
+        // stack.pop();
+
+        // Select without eliminate
+        String favoriteGame = stack.peek();
+
+        System.out.println(favoriteGame);
+        System.out.println(stack);
+
+        System.out.println("The position of the firts game is: " + stack.search("Borderlands"));
+
+        
+
+    }
+
+}
